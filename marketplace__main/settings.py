2cda0d91f353292a3768078f05c46816fe3a1110
ramosmaria67 <ramosm66167@gmail.com>
2026-05-26 01:20:59 -0700

    acomode los modelos

--- marketplace__main/settings.py
+++ marketplace__main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'marketplace__main.urls'
+AUTH_USER_MODEL = 'marketplace.User'
 
 TEMPLATES = [
     {
